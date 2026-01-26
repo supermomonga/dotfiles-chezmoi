4a7db8b3513914b387a31a53cbc94fdee6066903
supermomonga <377137+supermomonga@users.noreply.github.com>
2026-01-26 15:05:53 +0900

    mise for windows

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
 # XDG Base Directory
 # change vimrc dir for neovim
 $env:XDG_CONFIG_HOME = "$HOME\.config"
+$env:XDG_DATA_HOME = "$HOME\.local\share"
+$env:XDG_STATE_HOME = "$HOME\.local\state"
+
+# mise
+(&mise activate pwsh) | Out-String | Invoke-Expression
