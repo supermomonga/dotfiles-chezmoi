152878580fc5cb7d4b93041d03d8f39cac98d787
supermomonga <377137+supermomonga@users.noreply.github.com>
2026-02-01 12:29:24 +0900

    PSReadLine

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -13,3 +13,19 @@ if ($resolved) {
     $Env:Path = "$($resolved.Path);$Env:Path"
 }
 
+# Incremental search
+Set-PSReadLineKeyHandler -Key Ctrl+p -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Ctrl+n -Function HistorySearchForward
+
+# 行頭 / 行末
+Set-PSReadLineKeyHandler -Key Ctrl+a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Key Ctrl+e -Function EndOfLine
+
+# 削除系
+Set-PSReadLineKeyHandler -Key Ctrl+k -Function KillLine
+Set-PSReadLineKeyHandler -Key Ctrl+u -Function BackwardKillLine
+
+# 1文字移動
+Set-PSReadLineKeyHandler -Key Ctrl+f -Function ForwardChar
+Set-PSReadLineKeyHandler -Key Ctrl+b -Function BackwardChar
+
